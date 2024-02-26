f1fd68267535a849e5bbd2c059715e3b94281ffa
S0ulx3 <98002038+S0ulx3@users.noreply.github.com>
2024-02-26 06:46:24 +0100

    Create nms_V-2.0.sh
    Versión de NMS pero la forma de introducir la IP es directamente como parámetro.
    NMS version but the way to enter the IP is directly as a parameter.

---
+++ nms_P-v1.0.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+
+# Función Ctrl+C
+
+function ctrl_c(){
+
+clear
+
+        echo -e " \e[31m[!] Saliendo...\e[0m"
+
+tput cnorm; exit 1
+}
+
+# Ctrl+C
+
+trap ctrl_c INT
+
+
+# Función ayuda
+function ayuda(){
+clear
+    echo -e "\e[31mUso: ./nms.sh -i <ip>\n"
+    echo -e "Opciones:\n"
+    echo -e "  -i <ip>       Indica la dirección ip"
+    echo -e "Ejemplo:"
+    echo -e '  ./nms.sh -i 192.168.1.1 \e[0m\n'
+}
+
+# Escaneo de puertos
+
+clear
+while getopts i: option
+do
+case "${option}"
+in
+i) ADDRESS=${OPTARG};;
+*) ayuda; exit 1;;
+esac
+done
+
+clear
+echo -e "\n\e[31m-----------------"
+echo -e "- Escaneando...    "
+echo -e "------------------\e[0m"
+echo
+echo -e "\e[31m---------------------------------------------------------------------------------------------------------------\n\e[32m"
+
+nmap -p- --open -sSV -vvv -n $ADDRESS  | grep -w "PORT" -A 9999 | grep -w "Read data files" -B 9999
+
+
+echo -e "\n\e[31m---------------------------------------------------------------------------------------------------------------\e[0m"
+
+exit 1
+
+# Si no se introdujeron parámetros, muestra la ayuda
+
+if [ -z "ADDRESS" ]; then
+    ayuda
+    exit 1
+fi
